f284ad2a97aac8ce0d4c242311d03c1ae6facb53
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2024-02-29 22:59:30 +0530

    Learning Hono

--- my-app/src/index.ts
+++ my-app/src/index.ts
@@ -5,18 +5,6 @@ export interface Env {
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		console.log(request.body);
-		console.log(request.headers);
-
-		if (request.method == "GET") {
-			return Response.json({
-				message: "You sent a get request"
-			});
-		} else {
-			return Response.json({
-				message: "You did not send a get request"
-			})
-		}
 
 		return Response.json({
 			message: "Hi There"
