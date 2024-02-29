a21cecab636c8219beeda62f3200293f1bdd8189
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2024-02-29 23:06:01 +0530

    Writing Post request

--- hono-app/src/index.ts
+++ hono-app/src/index.ts
@@ -2,12 +2,12 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
-app.get('/', async(c) => {
+app.post('/', async(c) => {
   const body = await c.req.json()
   console.log(body);
   console.log(c.req.header("Authorization"));
   console.log(c.req.query("param"));
-  
+
   return c.text('Hello Hono!')
 })
 
