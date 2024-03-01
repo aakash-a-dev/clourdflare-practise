1cdaf1e42707611fd62c312000c5287b2f557f33
Aakash Amod Rajput <126223325+aakash-a-dev@users.noreply.github.com>
2024-03-01 20:02:55 +0530

    Created a middleware

--- hono-app/src/index.ts
+++ hono-app/src/index.ts
@@ -2,6 +2,15 @@ import { Hono } from 'hono'
 
 const app = new Hono()
 
+async function authMiddleware(c:any, next:any) {
+  if (c.req.header("Authorization")) {
+    await next();
+  } else {
+    return c.text("You don't have access")
+  }
+}
+
+app.use(authMiddleware);
 app.post('/', async(c) => {
   const body = await c.req.json()
   console.log(body);
